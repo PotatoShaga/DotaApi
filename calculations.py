8b33a88b287b42b793c22eea007829790d7e6a1c
PotatoShaga <stronguy991@gmail.com>
2024-11-20 17:55:49 -0500

    isonmyteam fix

--- calculations.py
+++ calculations.py
@@ -8,7 +8,7 @@ pd.options.display.max_columns = None
 
 #CONSTRUCTS df_calculated
 
-def adding_columns(df_raw,steam_id,minute):
+def adding_columns(df_raw,steam_id,minute, isOnMyTeam):
     important_raw_columns = ["id","playerSlot","position","heroId"]
     df_calculated = df_raw[important_raw_columns].copy()
     df_calculated["networthDifference"] = 0
@@ -16,14 +16,21 @@ def adding_columns(df_raw,steam_id,minute):
     for match_id in df_raw["id"].unique(): #agnostically gets raw data for everyone. more specific specification happens in player_calculations
         
 
-        def is_on_my_team_column(df_raw,match_id,steam_id): #for some reason these parameters arent needed inside this adding_columns function? like code works perfectly fine if u delete the parameters, ig cus parameters already gotten
+        def is_on_my_team_column(df_raw,match_id,steam_id, isOnMyTeam=True): #isOnMyTeam is variable in script to see if you actually want correct or inverted results
             radiant = df_raw.loc[(df_raw["steamAccountId"]==steam_id) & (df_raw["id"]==match_id),"isRadiant"].values[0]
-            if radiant == True:
-                df_raw.loc[df_raw["id"] == match_id,"isOnMyTeam"] = df_raw["isRadiant"]
-            else:
-                df_raw.loc[df_raw["id"] == match_id,"isOnMyTeam"] = ~df_raw["isRadiant"]
 
-        is_on_my_team_column(df_raw,match_id,steam_id)
+            if isOnMyTeam == True:
+                if radiant == True:
+                    df_raw.loc[df_raw["id"] == match_id,"isOnMyTeam"] = df_raw["isRadiant"]
+                else:
+                    df_raw.loc[df_raw["id"] == match_id,"isOnMyTeam"] = ~df_raw["isRadiant"]
+
+            elif isOnMyTeam == False:
+                if radiant == True:
+                    df_raw.loc[df_raw["id"] == match_id,"isOnMyTeam"] = ~df_raw["isRadiant"] #not operator gets swapped if ==false (so everything gets inverted)
+                else:
+                    df_raw.loc[df_raw["id"] == match_id,"isOnMyTeam"] = df_raw["isRadiant"]
+        is_on_my_team_column(df_raw,match_id,steam_id, isOnMyTeam)
 
 
         #first df is df_raw, second df is df_calculated 
@@ -112,7 +119,7 @@ def player_calculations(df_calculated):
 
 
     def networth_difference(df_calculated):
-        nw_dict = {  #Ally-Enemy 
+        nw_dict = {#Ally-Enemy 
             1:"" , #1 is 1-1
             2:"" , #2 is 2-2
             3:"" , #3 is 3-3
--- script.py
+++ script.py
@@ -16,18 +16,18 @@ pd.options.display.max_colwidth = None #for some fucking reason pandas truncates
 #steam_id = 405788540
 #steam_id = 171262902 #watson
 #steam_id = 898455820 #malrine
-#steam_id = 183719386 #atf
+steam_id = 183719386 #atf
 #steam_id = 360577618 #snlork
 #steam_id = 177658823 #off
-steam_id = 52023367 #hobbes
+#steam_id = 52023367 #hobbes
 
-position = "POSITION_1"
+position = "POSITION_3"
 isOnMyTeam = True #this is only used in player_graphs and worksheet string. by default its true for player_graphs
 "========================================================"
 duration = 20
 minute = 11 #MINUTE 11 BY DEFAULT. minute 11 is exactly 10:01
 skip_interval = 25
-number_of_matches_to_parse = 200 #accepts numbers 0-{skip_interval}, for numbers above it needs to be intervals of {skip_interval}
+number_of_matches_to_parse = 1 #accepts numbers 0-{skip_interval}, for numbers above it needs to be intervals of {skip_interval}
 "========================================================"
 
 def make_all_excel_sheets(): #just for ease of use, so i dont have to call every one seperately
@@ -55,7 +55,7 @@ def make_all_excel_sheets(): #just for ease of use, so i dont have to call every
     ws["A6"] = "The networthDifference stats are ordered by comparing the respective positions to themselves for index 1-5 (so index 3 is YourPos3-TheirPos3). Index 6-10 are comparing positions to their lane opposition (index 8 is YourPos3-TheirPos1)"
     ws["A6"].alignment = Alignment(wrap_text=True, vertical="top")
     ws["A7"] = "The stats above kills/deaths are the kda ratio"
-    ws["A8"] = "If levels/kda is empty that means stratz api returned no playbackData. Try parsing your latest match on stratz (idk if this will work)"
+    ws["A8"] = "If levels/kda is empty/lacking data that means stratz api returned no playbackData. Try parsing your latest match on stratz (idk if this will work)"
     ws["A8"].alignment = Alignment(wrap_text=True, vertical="top") 
 
     ws["B1"] = df_player_calculations["networth_difference"].to_string()
@@ -96,7 +96,7 @@ def make_all_excel_sheets(): #just for ease of use, so i dont have to call every
 df_raw = api_handler.queries_to_batches_main(steam_id, position, skip_interval, number_of_matches_to_parse)
 print(df_raw)
 
-df_calculated = calculations.adding_columns(df_raw,steam_id,minute) #this function turns df_raw into df_calculated
+df_calculated = calculations.adding_columns(df_raw, steam_id, minute, isOnMyTeam) #this function turns df_raw into df_calculated
 print("--------------------")
 print(df_calculated)
 
